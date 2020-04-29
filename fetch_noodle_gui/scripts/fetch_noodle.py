51d7f2e04d98369725bbd178e50b981c4d93e9c6
James Ward <j.ward@acfr.usyd.edu.au>
2020-04-29 14:56:05 +1000

    Add arm movements to operator GUI

--- fetch_noodle_gui/scripts/fetch_noodle.py
+++ fetch_noodle_gui/scripts/fetch_noodle.py
@@ -4,11 +4,40 @@ import rospy
 from Tkinter import *
 import ttk
 
+import actionlib
+from control_msgs.msg import FollowJointTrajectoryAction, GripperCommandAction
+from control_msgs.msg import FollowJointTrajectoryGoal, GripperCommandGoal
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from moveit_python import MoveGroupInterface
 from sound_play.libsoundplay import SoundClient
+
+
+class Actions:
+    def __init__(self):
+        self.arm = actionlib.SimpleActionClient("/arm_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        self.gripper = actionlib.SimpleActionClient("/gripper_controller/gripper_action", GripperCommandAction)
+        self.arm.wait_for_server()
+        self.gripper.wait_for_server()
+
+        self.open_gripper = GripperCommandGoal()
+        self.open_gripper.command.position = 1.0
+        self.close_gripper = GripperCommandGoal()
+        self.close_gripper.command.position = 0.0
+        self.close_gripper.command.max_effort = 10.0  # TODO check on real robot how hard to close
+
+        self.move_group = MoveGroupInterface("arm", "base_link")
+        self.arm_up_pose = PoseStamped()
+        self.arm_up_pose.header.frame_id = "base_link"
+        self.arm_up_pose.pose = Pose(Point(0.4, 0.0, 0.6), Quaternion(0.5,-0.5,0.5,0.5))
+        self.arm_down_pose = PoseStamped()
+        self.arm_down_pose.header.frame_id = "base_link"
+        self.arm_down_pose.pose = Pose(Point(0.4, 0.0, 0.5), Quaternion(0,0,0,1))
+
 
 class Gui:
     def __init__(self, master):
         self.sound = SoundClient()
+        self.actions = Actions()
 
         frame = Frame(master)
         frame.pack()
@@ -53,8 +82,11 @@ class Gui:
 
     def get_noodle_cmd(self):
         self.get_noodle["state"] = DISABLED
+        self.grab["state"] = DISABLED
         self.run["state"] = DISABLED
-        # TODO Open the gripper
+        self.actions.arm_up_pose.header.stamp = rospy.Time.now()
+        self.actions.move_group.moveToPose(self.actions.arm_up_pose, "wrist_roll_link")
+        self.actions.gripper.send_goal_and_wait(self.actions.open_gripper)
 
         # Play the request
         self.sound.say("Please place the end of the pool noodle in my hand.")
@@ -62,14 +94,15 @@ class Gui:
 
     def grab_cmd(self):
         self.grab["state"] = DISABLED
-        # TODO Close the gripper
+        self.get_noodle["state"] = DISABLED
+        self.actions.gripper.send_goal_and_wait(self.actions.close_gripper)
 
         self.get_noodle["state"] = NORMAL
         self.run["state"] = NORMAL
 
     def run_cmd(self):
-        self.get_noodle["state"] = DISABLED
         self.run["state"] = DISABLED
+        self.get_noodle["state"] = DISABLED
         self.sound.say("Lets go!")
         self.cancel["state"] = NORMAL
         # TODO Run the nav stack
@@ -77,16 +110,18 @@ class Gui:
         self.cancel["state"] = DISABLED
 
     def release_cmd(self):
+        self.release["state"] = DISABLED
         # Play the thank you
         self.sound.say("Thank you for your help. Please place the pool noodle in the pile.")
 
-        # TODO Open the gripper
+        self.actions.gripper.send_goal_and_wait(self.actions.open_gripper)
+        self.actions.arm_down_pose.header.stamp = rospy.Time.now()
+        self.actions.move_group.moveToPose(self.actions.arm_down_pose, "wrist_roll_link")
         self.reset["state"] = NORMAL
-        self.release["state"] = DISABLED
 
     def reset_cmd(self):
-        self.cancel["state"] = NORMAL
         self.reset["state"] = DISABLED
+        self.cancel["state"] = NORMAL
         # TODO Face back towards start
 
         # Ask subject to follow
@@ -107,7 +142,6 @@ def main():
     root.title ("Fetch Noodle")
 
     gui = Gui(root)
-
 
     root.mainloop()
 
